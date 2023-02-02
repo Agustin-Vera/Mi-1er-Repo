a55a861a6ddb1543855f9cddf7d9e3486a1c99dd
Agustin Vera <agustin.vera@usach.cl>
2023-02-02 20:00:45 -0300

    Cambio en la resolucion, ahora es Fibonacci iterativo. Ademas se muestra el grafico.

--- Sucesion_Fibonacci/fibonacci.py
+++ Sucesion_Fibonacci/fibonacci.py
@@ -1,19 +1,45 @@
-#Este programa imprime por pantalla la sucesion de fibonacci, de los n numeros que el usuario desee
+import matplotlib.pyplot as plt
 
-print("Este programa calcula el valor de la sucesion de Fibonacci")
+#Este programa imprime por pantalla la sucesión de fibonacci, de los n numeros que el usuario desee
+#Además imprime un gráfico mostrando dicha sucesión
+
+print("## Sucesion de Fibonacci ##")
 
 #CONSTANTES
 n = int(input("     Ingrese un numero para calcular la sucesion de fibonacci: "))
 
 #FUNCIONES
-#Fibonacci recursivo
-def fibonacci(n):
-    if n < 2:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+#Funcion que crea una lista de numeros con orden ascendente de 0 hasta "largo"
+def numeroAscendente(lista,largo):
+    i = 0
+    while(i<largo):
+        lista.append(i+1)
+        i = i+1
 
-#Main
-numeroFibonacci = fibonacci(n)
+#Fibonacci Iterativo
+def fibonacci(n, lista):
+    lista.append(1)
+    lista.append(1)
+    a = lista[0]
+    b = lista[1]
+    c = 0
+    i = 2
+    while(i<n):
+        lista.append(a+b)
+        a = b
+        b = lista[i]
+        i = i+1
+    return lista[i-1]
+        
 
-print("     El resultado de la sucesion de fibonacci hasta",n,"es:",numeroFibonacci)
+#MAIN
+lista_x = []
+lista_y = []
+numeroFibonacci = fibonacci(n, lista_x)
+numeroAscendente(lista_y, n)
+print("     El resultado de la sucesion de Fibonacci es:",numeroFibonacci)
+print("     Sucesión de Fibonacci hasta", n,":")
+print("    ",lista_x)
+
+plt.plot(lista_x, lista_y)
+plt.show()
